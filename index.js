d4c9fb26d069c5216b99127dbb76e453d908e91e
Gloria-Lado <glorialadoo@gmail.com>
2023-05-25 20:20:13 +0300

    files

---
+++ index.js
@@ -0,0 +1,35 @@
+// Change document background color to
+// silver
+
+document.getElementsByClassName("")
+
+// document.getElementsByClassName('container')
+// document.getElementById('container').style.color="red"
+// document.getElementById('container').style.backgroundColor ="#e2e2e2"
+// document.getElementById('text').innerHTML="My name is Gloria Lado and am 22 year of age"
+// document.getElementById('container').childNodes;
+
+
+// let child =document.getElementById('container').childNodes;
+// console.log({child})
+
+// let children =document.getElementById('container').children;
+// console.log({children})
+
+// let p =document.createElement("p");
+// p.innerHTML ="Am doing great"
+// document.getElementById('container').appendChild(p)
+// p.setAttribute("class","paragraph")
+// p.setAttribute("id","paragraph")
+// document.getElementById("paragraph").style.color ="green";
+
+
+// let buttun =document.getElementById('buttun')
+// buttun.addEventListener("click",function(){
+//     buttun.innerHTML="Clicked !!"
+//     buttun.style.backgroundColor="#19ba96"
+//     buttun.style.padding ="10px"
+//     buttun.style.borderRadius="5px"
+//     buttun.style.border ="none"
+// })
+
